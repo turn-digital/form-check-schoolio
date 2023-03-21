6d6205f48692cf50efa5cc29a9345acf1e1dcb76
Vladyslav Tkachenko <vlad@wtf.lv>
2023-03-21 14:27:03 +0200

    changing router

--- src/App.js
+++ src/App.js
@@ -4,41 +4,35 @@ import Hero from "./components/hero/Hero";
 import News from "./components/news/News";
 import Contact from "./Contact";
 import "./index.scss";
-import {
-  BrowserRouter as Router,
-  Routes as Switch,
-  Route,
-} from "react-router-dom";
+import { Routes as Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route
-          exect
-          path="/"
-          element={
-            <div className="home page__flexblock">
-              <Header />
-              <section className="home__section home__section--hero">
-                <Hero />
-              </section>
-              <News />
-              <Footer />
-            </div>
-          }
-        />
+    <Switch>
+      <Route
+        exect
+        path="/"
+        element={
+          <div className="home page__flexblock">
+            <Header />
+            <section className="home__section home__section--hero">
+              <Hero />
+            </section>
+            <News />
+            <Footer />
+          </div>
+        }
+      />
 
-        <Route
-          path="/Contact"
-          element={
-            <>
-              <Contact />
-            </>
-          }
-        />
-      </Switch>
-    </Router>
+      <Route
+        path="/Contact"
+        element={
+          <>
+            <Contact />
+          </>
+        }
+      />
+    </Switch>
   );
 }
 export default App;
